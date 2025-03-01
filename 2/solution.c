06b95196bfd10b91993609f91358ca5cf50349ca
Gregory <gmmishin@edu.hse.ru>
2025-03-01 14:48:08 +0300

    Исправленный коммит

--- 2/solution.c
+++ 2/solution.c
@@ -3,13 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#ifdef _WIN32
-#include <windows.h>
-#include <process.h>
-#define waitpid(pid, status, options) _cwait(status, pid, 0)
-#else
 #include <sys/wait.h>
-#endif
 #include <fcntl.h>
 #include <errno.h>
 #include <signal.h>
@@ -18,82 +12,75 @@
 #define APP  1
 
 static int xp(struct expr **cmds, int n, char *of, int ot, int bg) {
-    if(n == 1) {
+    if (n == 1) {
         char *c = cmds[0]->cmd.exe;
-        if(!strcmp(c, "cd")) {
-            char *d = (cmds[0]->cmd.arg_count > 0) ? cmds[0]->cmd.args[0] : getenv("HOME");
-            if(chdir(d) != 0)
-                perror("cd");
+        if (!strcmp(c, "cd")) {
+            char *d = cmds[0]->cmd.arg_count > 0 ? cmds[0]->cmd.args[0] : getenv("HOME");
+            if (chdir(d) != 0) perror("cd");
             return 0;
         }
-        if(!strcmp(c, "exit") && !of) {
+        if (!strcmp(c, "exit") && !of) {
             int ec = 0;
-            if(cmds[0]->cmd.arg_count > 0)
-                ec = atoi(cmds[0]->cmd.args[0]);
+            if (cmds[0]->cmd.arg_count > 0) ec = atoi(cmds[0]->cmd.args[0]);
             exit(ec);
         }
     }
     int i, ret = 0;
     int pfd[2], prev = -1;
     pid_t *pids = malloc(n * sizeof(pid_t));
-    for(i = 0; i < n; i++) {
-        if(i < n - 1) {
-            if(pipe(pfd) < 0) {
+    for (i = 0; i < n; i++) {
+        if (i < n - 1) {
+            if (pipe(pfd) < 0) {
                 perror("pipe");
                 exit(1);
             }
         }
         pid_t pid = fork();
-        if(pid < 0) {
+        if (pid < 0) {
             perror("fork");
             exit(1);
         }
-        if(pid == 0) {
-            if(prev != -1) {
+        if (pid == 0) {
+            if (prev != -1) {
                 dup2(prev, STDIN_FILENO);
                 close(prev);
             }
-            if(i < n - 1) {
+            if (i < n - 1) {
                 close(pfd[0]);
                 dup2(pfd[1], STDOUT_FILENO);
                 close(pfd[1]);
-            } else if(of) {
-                int fl = (ot == ONEW) ?
-                    open(of, O_WRONLY | O_CREAT | O_TRUNC, 0644) :
-                    open(of, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                if(fl < 0) {
+            } else if (of) {
+                int fl = (ot == ONEW) ? open(of, O_WRONLY | O_CREAT | O_TRUNC, 0644)
+                                      : open(of, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                if (fl < 0) {
                     perror("open");
                     exit(1);
                 }
                 dup2(fl, STDOUT_FILENO);
                 close(fl);
             }
-            if(!strcmp(cmds[i]->cmd.exe, "exit")) {
+            if (!strcmp(cmds[i]->cmd.exe, "exit")) {
                 int ec = 0;
-                if(cmds[i]->cmd.arg_count > 0)
-                    ec = atoi(cmds[i]->cmd.args[0]);
+                if (cmds[i]->cmd.arg_count > 0) ec = atoi(cmds[i]->cmd.args[0]);
                 _exit(ec);
             }
-            execvp(cmds[i]->cmd.exe, (char* const*)((char*[]){cmds[i]->cmd.exe, cmds[i]->cmd.arg_count > 0 ? cmds[i]->cmd.args[0] : NULL, NULL}));
-            execvp(cmds[i]->cmd.exe, cmds[i]->cmd.args);
+            execvp(cmds[i]->cmd.exe, (char * const *)cmds[i]->cmd.args);
             perror(cmds[i]->cmd.exe);
             _exit(1);
         } else {
             pids[i] = pid;
-            if(prev != -1)
-                close(prev);
-            if(i < n - 1) {
+            if (prev != -1) close(prev);
+            if (i < n - 1) {
                 close(pfd[1]);
                 prev = pfd[0];
             }
         }
     }
-    if(!bg) {
-        for(i = 0; i < n; i++) {
+    if (!bg) {
+        for (i = 0; i < n; i++) {
             int st;
             waitpid(pids[i], &st, 0);
-            if(i == n - 1)
-                ret = WEXITSTATUS(st);
+            if (i == n - 1) ret = WEXITSTATUS(st);
         }
     }
     free(pids);
@@ -103,12 +90,12 @@ static int xp(struct expr **cmds, int n, char *of, int ot, int bg) {
 static int exec_line(struct command_line *cl) {
     int ret = 0;
     struct expr *e = cl->head;
-    while(e) {
+    while (e) {
         int cnt = 0, cap = 4;
         struct expr **arr = malloc(cap * sizeof(struct expr*));
-        while(e && (e->type == EXPR_TYPE_COMMAND || e->type == EXPR_TYPE_PIPE)) {
-            if(e->type == EXPR_TYPE_COMMAND) {
-                if(cnt >= cap) {
+        while (e && (e->type == EXPR_TYPE_COMMAND || e->type == EXPR_TYPE_PIPE)) {
+            if (e->type == EXPR_TYPE_COMMAND) {
+                if (cnt >= cap) {
                     cap *= 2;
                     arr = realloc(arr, cap * sizeof(struct expr*));
                 }
@@ -116,18 +103,17 @@ static int exec_line(struct command_line *cl) {
             }
             e = e->next;
         }
-        ret = xp(arr, cnt, cl->out_file, (cl->out_type == OUTPUT_TYPE_FILE_NEW) ? ONEW : APP, cl->is_background);
+        ret = xp(arr, cnt, cl->out_file,
+                 (cl->out_type == OUTPUT_TYPE_FILE_NEW) ? ONEW : APP,
+                 cl->is_background);
         free(arr);
-        if(e && (e->type == EXPR_TYPE_AND || e->type == EXPR_TYPE_OR)) {
+        if (e && (e->type == EXPR_TYPE_AND || e->type == EXPR_TYPE_OR)) {
             int op = e->type;
             e = e->next;
-            if(op == EXPR_TYPE_AND && ret != 0) {
-                while(e && (e->type == EXPR_TYPE_COMMAND || e->type == EXPR_TYPE_PIPE))
-                    e = e->next;
-            }
-            else if(op == EXPR_TYPE_OR && ret == 0) {
-                while(e && (e->type == EXPR_TYPE_COMMAND || e->type == EXPR_TYPE_PIPE))
-                    e = e->next;
+            if (op == EXPR_TYPE_AND && ret != 0) {
+                while (e && (e->type == EXPR_TYPE_COMMAND || e->type == EXPR_TYPE_PIPE)) e = e->next;
+            } else if (op == EXPR_TYPE_OR && ret == 0) {
+                while (e && (e->type == EXPR_TYPE_COMMAND || e->type == EXPR_TYPE_PIPE)) e = e->next;
             }
         }
     }
@@ -139,18 +125,17 @@ int main(void) {
     struct parser *p = parser_new();
     char buf[1024];
     int r;
-    while((r = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+    while ((r = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
         parser_feed(p, buf, r);
         struct command_line *cl = NULL;
-        while(1) {
+        while (1) {
             enum parser_error err = parser_pop_next(p, &cl);
-            if(err == PARSER_ERR_NONE && cl == NULL)
-                break;
-            if(err != PARSER_ERR_NONE) {
-                fprintf(stderr, "Error: %d\n", (int)err);
+            if (err == PARSER_ERR_NONE && cl == NULL) break;
+            if (err != PARSER_ERR_NONE) {
+                fprintf(stderr, "Ошибка парсера: %d\n", (int)err);
                 continue;
             }
-            int ec = exec_line(cl);
+            exec_line(cl);
             command_line_delete(cl);
         }
     }
